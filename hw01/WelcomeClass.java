837dc8b55eae49199fc10e93470bacfaad50d7a9
mrk418 <mrk418@lehigh.edu>
2014-08-31 20:23:17 +0000

    WelcomeClass program

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+///////////////////////////////////////
+//Matt Kawa
+//HW 01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+
+//  add main method
+    public static void main(String[] args) {
+    
+//      print the statement
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-M--R--K--4--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("At the beginning of this summer, I received my third-degree black belt in Kenpo Karate.");
+        
+
+    }
+}
